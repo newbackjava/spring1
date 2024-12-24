326af0454351dd35a5b68cb81d1530206f73f1c7
newbackjava <hera@naver.com>
2024-12-24 17:26:57 +0900

    Initial commit

---
+++ src/main/java/org/example/spring1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.example.spring1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(org.example.spring1.Spring1Application.class);
+    }
+
+}
---
+++ src/main/java/org/example/spring1/controller/HomeController.java
@@ -0,0 +1,34 @@
+package org.example.spring1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //mvc중에서 컨트롤러 역할을 담당하는 것으로 설정.
+//주소 + method를 인식해서 처리하는 함수를 호출하는 코드!
+public class HomeController {
+
+    //요청하나당 함수하나 get방식+test주소
+    @GetMapping("test")
+    public String test(){ //함수이름은 마음대로!
+        System.out.println("test get요청 들어옴.");
+        return "test";
+        //html파일 리턴!!! test.html파일을 호출해서 응답
+    }
+
+    //요청하나당 함수하나 get방식+member주소
+    @GetMapping("member")
+    public String member(){ //함수이름은 마음대로!
+        System.out.println("member get요청 들어옴.");
+        return "member";
+        //member.html파일을 리턴
+    }
+
+    //요청하나당 함수하나 get방식+member주소
+    @GetMapping("board")
+    public String board(){ //함수이름은 마음대로!
+        System.out.println("board get요청 들어옴.");
+        return "board";
+        //board.html파일을 리턴
+    }
+
+}
